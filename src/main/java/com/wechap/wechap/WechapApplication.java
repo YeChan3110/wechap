a39311a0cf4e78caac2d4a6b3fd0e30ad193eb9a
yechan <yechan3110@gmail.com>
2023-04-09 01:53:22 +0900

    start

---
+++ src/main/java/com/wechap/wechap/WechapApplication.java
@@ -0,0 +1,13 @@
+package com.wechap.wechap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wechap/wechap/WechapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wechap.wechap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
