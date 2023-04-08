a4c46252f775e99bab556ecdb3fb57c46b8d59e5
yechan <yechan3110@gmail.com>
2023-04-09 03:31:02 +0900

    (ver 0.0.0 세팅)

--- src/main/java/com/wechap/wechap/WechapApplication.java
+++ src/main/java/com/wechap/wechap/WechapApplication.java
@@ -1,7 +1,11 @@
 package com.wechap.wechap;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 public class WechapApplication {
---
+++ src/main/java/com/wechap/wechap/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.wechap.wechap.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    void test(int test);
+}
