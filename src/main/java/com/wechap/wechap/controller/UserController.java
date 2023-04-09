a515862e3b2d1b172aec6d85e860a246ad1acfd9
yechan <yechan3110@gmail.com>
2023-04-10 03:16:02 +0900

    Refactor : 로그인페이지 분리로 인한 이동경로 추가

--- src/main/java/com/wechap/wechap/controller/UserController.java
+++ src/main/java/com/wechap/wechap/controller/UserController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("user")
 public class UserController {
+    @Autowired
+    private UserService us;
 
     // ExceptionHandler
     @ExceptionHandler(Exception.class)
@@ -19,16 +21,18 @@ public class UserController {
         return "/error";
     }
 
-    @Autowired
-    private UserService us;
+    @RequestMapping("toLogin") // 로그인 페이지로 이동
+    public String toLogin() {
+        return "user/login";
+    }
 
-    @RequestMapping("toSignUp")
-    public String toSignUp() throws Exception{
+    @RequestMapping("toSignUp") // 회원가입 페이지로 이동
+    public String toSignUp() {
         return "user/signUp";
     }
 
-    @PostMapping("signUp")
-    public String signUp(UserDTO userDto) throws Exception{
+    @PostMapping("signUp") // 회원가입
+    public String signUp(UserDTO userDto) {
         us.signUp(userDto);
         return "redirect:/";
     }
