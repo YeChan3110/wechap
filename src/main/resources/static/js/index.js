eb50a645e65373cde8e2a3024daf06c544f912d7
yechan <yechan3110@gmail.com>
2023-04-12 08:59:48 +0900

    Feat : 페이지 이동 js

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,9 @@
+// 날씨, 위치 페이지 이동
+$('#weather').on('click',function(){
+    window.location.href = '/weather/goWeather';
+})
+
+// 로그인 페이지 이동
+$('#login').on('click',function(){
+    window.location.href = '/user/toLogin';
+})
