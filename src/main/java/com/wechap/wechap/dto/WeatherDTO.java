a07f1ac47566be1ec0aeb66c31d7867bd4d5838e
yechan <yechan3110@gmail.com>
2023-04-12 09:08:36 +0900

    Refactor : 불필요한 주석 제거, getter,setter,constructor 어노테이션으로 교체

--- src/main/java/com/wechap/wechap/dto/WeatherDTO.java
+++ src/main/java/com/wechap/wechap/dto/WeatherDTO.java
@@ -15,42 +15,19 @@ public class WeatherDTO {
     private WeatherMainDto main;
     private List<WeatherInfoDto> weather;
 
-    // getter, setter 생략
-
+    @Getter
+    @Setter
+    @NoArgsConstructor
     public static class WeatherMainDto {
         private double temp;
-
-        // getter, setter 생략
-
-        public double getTemp() {
-            return temp;
-        }
-
-        public void setTemp(double temp) {
-            this.temp = temp;
-        }
     }
 
+    @Getter
+    @Setter
+    @NoArgsConstructor
     public static class WeatherInfoDto {
         private String description;
+
         private String icon;
-
-        // getter, setter 생략
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public String getIcon() {
-            return icon;
-        }
-
-        public void setIcon(String icon) {
-            this.icon = icon;
-        }
     }
 }
