9f5a6fa6b84ed1905e6661f8e8c2c5746b65799c
yechan <yechan3110@gmail.com>
2023-04-12 09:08:04 +0900

    Feat : getWeather 메서드에 주소 추가 현재 도시명은 Seoul로 고정해놨으나,추후 위도 경도를 받아와서 도시명을 파라미터로 넣어줄 것. 현재는 로컬에서 가동하기 때문에 https가 아니라서 위치를 받아올 수 없음.

--- src/main/java/com/wechap/wechap/controller/WeatherController.java
+++ src/main/java/com/wechap/wechap/controller/WeatherController.java
@@ -1,27 +1,50 @@
 package com.wechap.wechap.controller;
 
-import com.wechap.wechap.dto.WeatherDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.wechap.wechap.dto.GeocodeDTO;
+import com.wechap.wechap.dto.Weather2DTO;
+import com.wechap.wechap.service.MapService;
 import com.wechap.wechap.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("weather")
 public class WeatherController {
-    private final WeatherService weatherService;
 
-    public WeatherController(WeatherService weatherService) {
-        this.weatherService = weatherService;
-    }
+    @Autowired
+    private WeatherService weatherService;
 
-    @GetMapping("/")
-    public String getWeather(Model model) {
-        WeatherDTO weatherDto = weatherService.getWeather("Seoul");
+    @Autowired
+    private MapService mapService;
 
-        model.addAttribute("temperature", weatherDto.getMain().getTemp());
-        model.addAttribute("description", weatherDto.getWeather().get(0).getDescription());
-        model.addAttribute("icon", weatherDto.getWeather().get(0).getIcon());
+    @GetMapping("goWeather")
+    public String getWeather(Model model) throws JsonProcessingException {
+        // 도시는 naverMapsApi에서 받아온걸로 파라미터 수정할 것
+
+        // 날씨 정보
+        Weather2DTO weather2Dto = weatherService.getWeather("Seoul");
+
+        model.addAttribute("temperature", weather2Dto.getTemp());
+        model.addAttribute("description", weather2Dto.getDescription());
+        model.addAttribute("time", weather2Dto.getFormattedTime());
+
+        // 위도 경도로 주소
+        GeocodeDTO geocodeDto = mapService.getAdress();
+
+        model.addAttribute("country",geocodeDto.getCountry());
+        model.addAttribute("city",geocodeDto.getCity());
+        model.addAttribute("district",geocodeDto.getDistrict());
 
         return "recommend/weather";
     }
+
+    // 아래 코드는 WeatherDTO에서 사용
+    //        WeatherDTO weatherDto = weatherService.getWeather("Seoul");
+    //        model.addAttribute("temperature", weatherDto.getMain().getTemp());
+    //        model.addAttribute("description", weatherDto.getWeather().get(0).getDescription());
+    //        model.addAttribute("icon", weatherDto.getWeather().get(0).getIcon());
 }
