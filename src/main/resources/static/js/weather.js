7caf797620498038c2acb4767537ee52a55f3fbe
yechan <yechan3110@gmail.com>
2023-04-12 09:04:06 +0900

    Remove

--- src/main/resources/static/js/weather.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var city = document.querySelector('#city').value;
-    window.location.href = '/weather?city=' + encodeURIComponent(city);
-});
